116d12780717d6a66a1030c19bb5369affbbe9a7
khairan <adedoyinkhairan014@gmail.com>
2023-05-28 14:26:40 +0100

    Added components

---
+++ src/pages/Welcome/forgotPassword/LeftSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from "../components/Logo";
+import MainHeading from  "../components/MainHeading";
+import MainCaption from "../components/MainCaption";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import "../components/ReusableComponents.css";
+
+const LeftSection = ()=>{
+    return(
+        <>
+        <div className="leftSection">
+        <Logo />
+        <div>
+        <MainHeading heading="Reset Password"/>
+        <MainCaption caption="Enter the email associated with your account and we will send you a code to reset it."/>
+        <Input text="Email Address"/>
+        <Button text="Get code"/>
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default LeftSection;
--- src/pages/Welcome/forgotPassword/forgotPassword.jsx
+++ src/pages/Welcome/forgotPassword/forgotPassword.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import SideImage from "images/verification-image.png"
+import LeftSection from "./LeftSection";
+import RightSection from "../components/RightSection";
+import "./forgotPassword.css"
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -9,53 +11,21 @@ const ForgotPassword = () => {
     e.preventDefault();
   };
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setMessage("");
-    }, 3000);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setMessage("");
+  //   }, 3000);
 
-    return () => clearTimeout(timer);
-  }, [message]);
+  //   return () => clearTimeout(timer);
+  // }, [message]);
 
   return (
-    <div className="forgot-flex">
-
-      <div className="all">
-
-        <header className="logo-header">
-          <h1>Mealy</h1>
-        </header>
-
-        <div className="form-container">
-          <div className="form-minScreen">
-            <div className="text">
-              <h4 className="form-title">Forgot your Password?</h4>
-              <p className="form-description">
-                Enter the email address associated with your account
-              </p>
-            </div>
-            <div className="input">
-              <form onSubmit={handleSubmit}>
-                <Input
-                  type="email"
-                  className="input-width"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Email address"
-                  required
-                />
-                <Button type="submit" className="input-width">
-                  Get Code
-                </Button>
-              </form>
-            </div>
-            {message && <p className="message">{message}</p>}
-          </div>
-        </div>
-
-      </div>
-      <SideImage className="Simage" />
-    </div>
+   <>
+   <div className="container">
+    <LeftSection />
+    <RightSection />
+   </div>
+   </>
   );
 };
 
