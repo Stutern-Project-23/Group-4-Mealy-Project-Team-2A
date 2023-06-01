752f76aee8484e01821e3bdbedd614a6caac9563
RITAADEDOLAPO <ritaadedolapo2@gmail.com>
2023-06-01 20:23:30 +0100

    adjusted with tailwind

--- src/pages/signIn/SocialMedia.js
+++ src/pages/signIn/SocialMedia.js
@@ -5,15 +5,17 @@ import facebook from './SocialMediaIcons/facebook.png'
 
 const SocialMedia = () => {
   return (
-    <div className='social-media-wrapper'>
+    <div>
         <p>Or</p><p/>
         <p className='sign-in-using'>Sign In using</p>
-        <div className='social-media-icons'>
-          <a href="/"><img src={apple} alt="apple icon"/></a>
-          <a href="/"><img src={google} alt="google icon"/></a>
-          <a href="/"><img src={facebook} alt="facebook icon"/></a>
+        <div className='social-media-icons flex flex-row'>
+          <a href="/"><img src={apple} alt="apple icon" className='mx-5 mt-6 mb-10'/></a>
+          <a href="/"><img src={google} alt="google icon" className='mx-5 mt-6 mb-10'/></a>
+          <a href="/"><img src={facebook} alt="facebook icon" className='mx-5 mt-6 mb-10'/></a>
         </div>
-        <p className='account-creation-wrapper'>Don't have an account? <span className='account'>Sign Up</span></p>
+        <p>Don't have an account? <span className='text-orange-600 font-bold'>Sign Up</span></p>
+        
+        
     </div>
   )
 }
