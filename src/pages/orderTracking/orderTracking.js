660c1cfcfdff580618a51f4e98f9d8853f30f412
khairan <adedoyinkhairan014@gmail.com>
2023-07-08 15:18:17 +0100

    order tracking

--- src/pages/orderTracking/orderTracking.js
+++ src/pages/orderTracking/orderTracking.js
@@ -1,7 +1,11 @@
 import React from "react";
-import  Layout from "../../components/LayoutAlt"
+import Layout from "../../components/LayoutAlt";
 import CircleProgressBar from "./circle";
 import Map2 from "../../assets/map2.svg";
+import Profile from "../../assets/profile.png";
+import FullStar from "../../assets/fullStar.svg";
+import Star from "../../assets/Star.svg";
+import Call from "../../assets/call.svg";
 
 
 const progressMinutes = 5;
@@ -11,10 +15,13 @@ const OrderTracking = () =>{
         <>
         <Layout>
         <div>
-          <h2 className="font-bold">Your Mealy is being delivered.</h2>
+
+          <h2 className="font-bold pt-10">Your Mealy is being delivered.</h2>
+
           <div className="flex justify-center"> 
               <img src={Map2} alt="Map" className="w-[50%]" />
           </div>
+
           <div className="flex justify-center my-5">
           <CircleProgressBar 
            progressMinutes={progressMinutes}
@@ -25,6 +32,29 @@ const OrderTracking = () =>{
            progressColor="#fa5a00"
            textColor="black"
           />
+          </div>
+
+          <div className="flex space-x-3 justify-center">
+            <div className="flex space-x-4">
+              <img src={Profile} alt="driver-profile" className="w-[5%]"/>
+
+              <div className="flex flex-col">
+                <div>
+                  <p className="font-semibold">Fayemi Seun</p>
+                </div>
+                <div className="flex flex-row ">
+                <img src={FullStar} alt="star-rating" className="w-[50%]"/>
+                <img src={FullStar} alt="star-rating" className="w-[50%]"/>
+                <img src={FullStar} alt="star-rating" className="w-[50%]"/>
+                <img src={FullStar} alt="star-rating" className="w-[50%]"/>
+                <img src={Star} alt="star-rating"/> 
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <img src={Call} alt="call-phone" className="w-[75%]"/>
+            </div>
           </div>
 
         </div>
