976127422ec288c0727fc21db2c51ec9a9736d84
khairan <adedoyinkhairan014@gmail.com>
2023-07-10 12:34:46 +0100

    Order-tracking

--- src/pages/orderTracking/orderTracking.js
+++ src/pages/orderTracking/orderTracking.js
@@ -7,68 +7,67 @@ import FullStar from "../../assets/fullStar.svg";
 import Star from "../../assets/Star.svg";
 import Call from "../../assets/call.svg";
 
+
 const progressMinutes = 5;
 const totalMinutes = 10;
-const OrderTracking = () => {
-  return (
-    <>
-      <Layout>
-        <div>
-<<<<<<< HEAD
+const OrderTracking = () =>{
+    return(
+        <>
+        <Layout>
+        <div className="pt-9">
 
-          <h2 className="font-bold pt-10">Your Mealy is being delivered.</h2>
+          <div>
+          <h2 className="font-bold text-xl">Your Mealy is being delivered.</h2>
+          </div>
 
           <div className="flex justify-center"> 
               <img src={Map2} alt="Map" className="w-[50%]" />
-=======
-          <h2 className="font-bold">Your Mealy is being delivered.</h2>
-          <div className="flex justify-center">
-            <img src={Map2} alt="Map" className="w-[50%]" />
->>>>>>> b96576453949e5789459582552cecb063e0e8027
           </div>
 
           <div className="flex justify-center my-5">
-            <CircleProgressBar
-              progressMinutes={progressMinutes}
-              totalMinutes={totalMinutes}
-              size={75}
-              strokeWidth={4}
-              backgroundColor="#ccc"
-              progressColor="#fa5a00"
-              textColor="black"
-            />
+          <CircleProgressBar 
+           progressMinutes={progressMinutes}
+           totalMinutes={totalMinutes}
+           size={75}
+           strokeWidth={5}
+           backgroundColor="#ccc"
+           progressColor="#fa5a00"
+           textColor="black"
+          />
           </div>
-<<<<<<< HEAD
 
-          <div className="flex space-x-3 justify-center">
-            <div className="flex space-x-4">
-              <img src={Profile} alt="driver-profile" className="w-[5%]"/>
+          <div className="flex justify-center align-center gap-x-5">
+
+            <div className="flex">
+              <div>
+              <img src={Profile} alt="driver-profile" className="w-[70%]"/>
+              </div>
 
               <div className="flex flex-col">
                 <div>
                   <p className="font-semibold">Fayemi Seun</p>
                 </div>
                 <div className="flex flex-row ">
-                <img src={FullStar} alt="star-rating" className="w-[50%]"/>
-                <img src={FullStar} alt="star-rating" className="w-[50%]"/>
-                <img src={FullStar} alt="star-rating" className="w-[50%]"/>
-                <img src={FullStar} alt="star-rating" className="w-[50%]"/>
-                <img src={Star} alt="star-rating"/> 
+                <img src={FullStar} alt="star-rating" className="w-[20%]"/>
+                <img src={FullStar} alt="star-rating" className="w-[20%]"/>
+                <img src={FullStar} alt="star-rating" className="w-[20%]"/>
+                <img src={FullStar} alt="star-rating" className="w-[20%]"/>
+                <img src={Star} alt="star-rating" className="w-[20%]" /> 
                 </div>
               </div>
             </div>
 
             <div>
-              <img src={Call} alt="call-phone" className="w-[75%]"/>
+              <img src={Call} alt="call-phone" className="pt-2"/>
             </div>
           </div>
 
-=======
->>>>>>> b96576453949e5789459582552cecb063e0e8027
         </div>
-      </Layout>
-    </>
-  );
+      
+
+        </Layout>
+        </>
+    )
 };
 
 export default OrderTracking;
